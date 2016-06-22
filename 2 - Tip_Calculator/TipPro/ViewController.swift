b1656ddd2a855703df4554111e02a550a37a938b
Kenny Batista <kennybatista@Tests-MacBook-Pro.local>
2016-06-22 16:03:48 -0400

    New Files

---
+++ 2 - Tip_Calculator/TipPro/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  TipPro
+//
+//  Created by Kenny Batista on 6/22/16.
+//  Copyright © 2016 Kenny Batista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    @IBOutlet weak var tipAmountField: UITextField!
+    @IBOutlet weak var totalAmountField: UITextField!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func calculateTip(sender: UIButton) {
+        //the guard returns an error if the fields are empty, and if we press calculate when it's empty, it will empty out all of the other fields, so there wo't be cluster
+        guard let billAmount = Double(billAmountField.text!) else {
+            //show error
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAmountField.text = ""
+            return
+        }
+        
+        //setting initial value for percentage
+        var tipPercentage = 0.0
+        
+        //setting values for the segment index
+        switch tipSelector.selectedSegmentIndex {
+        case 0:
+            tipPercentage = 0.15
+        case 1:
+            tipPercentage = 0.18
+        case 2:
+            tipPercentage = 0.20
+        default:
+            break
+        }
+        
+        let roundedBillAmount = round(100*billAmount)/100
+        let tipAmount = roundedBillAmount * tipPercentage
+        let roundedTipAmount = round(100*tipAmount)/100
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        
+        if (!billAmountField.editing) {
+            billAmountField.text = String(format: "%2f", roundedBillAmount)
+        }
+        tipAmountField.text = String(format: "%2f", roundedBillAmount)
+        totalAmountField.text = String(format: "%2f", roundedBillAmount)
+    }
+
+
+}
+
+/*
+@IBAction func calculateTip(sender: AnyObject) {
+    guard let billAmount = Double(billAmountField.text!) else {
+        //show error
+        billAmountField.text = ""
+        tipAmountField.text = ""
+        totalAmountField.text = ""
+        return
+    }
+    
+    var tipPercentage = 0.0
+    
+    switch tipSelector.selectedSegmentIndex {
+    case 0:
+        tipPercentage = 0.15
+    case 1:
+        tipPercentage = 0.18
+    case 2:
+        tipPercentage = 0.20
+    default:
+        break
+    }
+    
+    let roundedBillAmount = round(100*billAmount)/100
+    let tipAmount = roundedBillAmount * tipPercentage
+    let roundedTipAmount = round(100*tipAmount)/100
+    let totalAmount = roundedBillAmount + roundedTipAmount
+    
+    if (!billAmountField.editing) {
+        billAmountField.text = String(format: "%.2f", roundedBillAmount)
+    }
+    tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+    totalAmountField.text = String(format: "%.2f", totalAmount)
+}
+ 
+ */
