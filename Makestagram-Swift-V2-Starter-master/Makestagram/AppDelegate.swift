e2729c08d58df87dcab40b5eaf0ed74211af326e
Kenny Batista <kennybatista@Tests-MacBook-Pro.local>
2016-07-01 16:04:13 -0400

    updating delegate

--- Makestagram-Swift-V2-Starter-master/Makestagram/AppDelegate.swift
+++ Makestagram-Swift-V2-Starter-master/Makestagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,9 +17,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-
+        
+        //Set up the Parse SDK:
+        let configuration = ParseClientConfiguration {
+            $0.applicationID = "makestagram"
+            $0.server = "https://makestagram-kb.herokuapp.com/parse"
+        }
+        
+        //calling the
+        Parse.initializeWithConfiguration(configuration)
         return true
     }
+    
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
